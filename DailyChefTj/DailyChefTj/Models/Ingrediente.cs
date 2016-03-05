1f4eb4a5081cf175ef5e9bcb89b2ecc7932a9036
GILBERTO GERALDO ROMERO <gilgr2k@hotmail.com>
2016-03-04 22:15:37 -0800

    CleanStart

---
+++ DailyChefTj/DailyChefTj/Models/Ingrediente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyChefTj.Models
+{
+    public class Ingrediente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public Proveedor Proveedor { get; set; }
+       
+    }
+}
