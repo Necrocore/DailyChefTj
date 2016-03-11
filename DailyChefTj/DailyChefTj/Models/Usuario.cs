e446e3e76c1f1088bba39b644c0530fb01d56afe
GILBERTO GERALDO ROMERO <gilgr2k@hotmail.com>
2016-03-11 15:29:35 -0800

    Compartir INICIO

---
+++ DailyChefTj/DailyChefTj/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DailyChefTj.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public List<Medidas> Medidas { get; set; }
+        public Rol Rol { get; set; }
+        public MetodoPago MetodoPago { get; set; }
+        public List<Ingrediente> IngredientesNoDeseados { get; set; }
+        public List<Ingrediente> Alergias { get; set; }
+    }
+}
